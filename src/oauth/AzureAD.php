c95f3c5a23f49ec5dd35062c6e6cf9f3fc12bc27
Ivan Bozhanov <git@vakata.com>
2017-09-16 17:09:48 +0300

    added tenant option to azureAD

--- src/oauth/AzureAD.php
+++ src/oauth/AzureAD.php
@@ -4,7 +4,14 @@ namespace vakata\authentication\oauth;
 class AzureAD extends OAuth
 {
 	protected $permissions  = 'https://graph.microsoft.com/User.Read';
-	protected $authorizeUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?';
+	protected $authorizeUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?response_type=id_token&';
 	protected $tokenUrl     = 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
 	protected $infoUrl      = 'https://graph.microsoft.com/v1.0/me?';
+
+	public function __construct($publicKey, $privateKey, $callbackUrl, $permissions = null, $tenant = 'common')
+	{
+		$this->authorizeUrl = str_replace('/common/', $tenant, $this->authorizeUrl);
+		$this->tokenUrl     = str_replace('/common/', $tenant, $this->tokenUrl);
+		parent::__construct($publicKey, $privateKey, $callbackUrl, $permissions);
+	}
 }
