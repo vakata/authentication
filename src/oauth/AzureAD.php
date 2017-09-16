1c284ac3340fd662911c4eed653944357ff33889
Ivan Bozhanov <git@vakata.com>
2017-09-16 16:58:17 +0300

    fixed default permissions in AzureAD

--- src/oauth/AzureAD.php
+++ src/oauth/AzureAD.php
@@ -3,7 +3,7 @@ namespace vakata\authentication\oauth;
 
 class AzureAD extends OAuth
 {
-	protected $permissions  = 'wl.signin,wl.basic';
+	protected $permissions  = 'https://graph.microsoft.com/User.Read';
 	protected $authorizeUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?';
 	protected $tokenUrl     = 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
 	protected $infoUrl      = 'https://graph.microsoft.com/v1.0/me?';
