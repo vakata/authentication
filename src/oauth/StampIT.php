dbca1576f99e2e6bdbca30032bbc8a20b55251e2
Ivan Bozhanov <git@vakata.com>
2019-11-29 16:55:32 +0200

    added StampIT OAuth class

---
+++ src/oauth/StampIT.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace vakata\authentication\oauth;
+
+class StampIT extends OAuth
+{
+    protected $permissions  = 'pid,name,mail,organization';
+    protected $authorizeUrl = 'https://id.stampit.org/authorize?';
+    protected $tokenUrl     = 'https://id.stampit.org/access_token';
+    protected $infoUrl      = 'https://id.stampit.org/me?';
+    protected $grantType    = '';
+
+    protected function extractUserData(array $data) : array
+    {
+        return [
+            'name'          => $data['name'] ?? null,
+            'mail'          => $data['mail'] ?? null,
+            'egn'           => $data['egn'] ?? null,
+            'bulstat'       => $data['bulstat'] ?? null,
+            'organization'  => $data['organization'] ?? null
+        ];
+    }
+    protected function extractUserID(array $data)
+    {
+        return $data['certno'] ?? null;
+    }
+}
